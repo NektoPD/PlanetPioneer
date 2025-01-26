7e64c6890020239629f32965ad228e54be58387e
NektoPD <nikita.averchenko1@gmail.com>
2025-01-26 11:04:09 +0300

    Removed unused using

--- Assets/Prefabs/ScenesTrasition/MainMenu.cs
+++ Assets/Prefabs/ScenesTrasition/MainMenu.cs
@@ -1,5 +1,4 @@
 using System;
-using Agava.YandexGames;
 using UnityEngine;
 using UnityEngine.UI;
 using YG;
--- Assets/Sources/PlayerResourceHandler/IResourceCatcher.cs
+++ Assets/Sources/PlayerResourceHandler/IResourceCatcher.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Agava.YandexGames;
 
 public interface IResourceCatcher
 {
